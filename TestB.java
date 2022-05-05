c2df1f5e4d4ea341d05351e0244cfb63dade1b59
velu <velu_b4u@yahoo.com>
2022-05-05 11:42:13 +0530

    Third commit

--- TestB.java
+++ TestB.java
@@ -1,7 +1,11 @@
 public class TestB 
 {
-    public static void main( String[] args )
-    {
+	public static void main(String args[]){
+		TestB test = new TestB();
+		test.methodA();
+	}
+	
+    void methodA() {
         System.out.println( "Hello Remote World!" );
     }
 }
